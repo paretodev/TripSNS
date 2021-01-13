8c60df124cb288da8f0b0b3e925ad54ab95cb4f2
한석희 <7616tjrgml@hanmail.net>
2021-01-13 11:20:31 +0900

    스플래시 - 메인 - 로그인 레이아웃 완성

--- TravelTest2/TravelTest2/LogInViewController.swift
+++ TravelTest2/TravelTest2/LogInViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var motherStackView: UIStackView!
+    @IBOutlet weak var identificationStackView: UIStackView!
+    @IBOutlet weak var findPasswordButtonStackView: UIStackView!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func backButtonPressed(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+        okButton.backgroundColor = .white
+        okButton.layer.cornerRadius = 8
+        //
+        motherStackView.setCustomSpacing(15, after: identificationStackView)
+        motherStackView.setCustomSpacing(50, after: findPasswordButtonStackView)
+        //
+        idTextField.attributedPlaceholder = NSAttributedString(string: "UserID", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        // Do any additional setup after loading the view.
     }
-    */
 
 }
