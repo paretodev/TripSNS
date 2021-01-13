6d535138a4d9a84c819998185ff6eda25c818ff5
한석희 <7616tjrgml@hanmail.net>
2021-01-13 10:43:50 +0900

    메인 화면 레이아웃 완성

---
+++ TravelTest2/TravelTest2/LogInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInViewController.swift
+//  TravelTest2
+//
+//  Created by 한석희 on 1/13/21.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
